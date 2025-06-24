0322c64c66bbfb6944f06ffe29b69d7dafc81c7e
Mariane Anjos <mariane.ferreiraanjos@outlook.com>
2025-06-24 15:36:22 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/mariane/petclinic/petclinic_api/PetclinicApiApplication.java
@@ -0,0 +1,13 @@
+package com.mariane.petclinic.petclinic_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariane/petclinic/petclinic_api/PetclinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariane.petclinic.petclinic_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
